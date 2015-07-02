f941dfb449c8e2c1dba13ad04ca6b1b48219be22
mattocci27 <mattocci27@gmail.com>
2015-07-02 13:54:06 -0400

    test
    test

---
+++ R/readtext.ij.R
@@ -0,0 +1,22 @@
+readtext.ij <-function(path){
+	file.list<-list.files(path)
+	file.list <- file.list[grep(".txt$",file.list)]
+	if (length(file.list[-grep("macro|bat",file.list)]!=0)) file.list <- file.list[-grep("macro|bat",file.list)]
+
+	size<-length(0)
+	file.name<-length(0)
+
+	data <- list()
+	for (i in 1:length(file.list))
+	{
+	temp <- read.delim(paste(path,file.list[i],sep=""))
+	data[[i]] <- data.frame(Area=temp$Area)
+	# data[[i]] <- read.delim(paste(path,file.list[i],sep=""))
+	names(data)[i]<-paste(file.list[i])
+	}
+	return(data)
+	}
+
+
+
+
---
+++ man/leafdata.rd
@@ -0,0 +1,13 @@
+\name{leafdata}
+\alias{leafdata}
+\docType{data}
+\title{ LeafArea default data }
+\description{
+  Analazed leafarea data for example
+}
+\usage{data(leafdata)}
+\format{
+    A list with six data frames, A1-01.jpeg.txt, A1-02.jpeg.txt, A123-01.jpeg.txt, A123-02.jpeg.txt, A2.jpeg.txt and A300-1.jpeg.txt. The first colmun is the identiy of image, and the second colmun is the are of the image (cm2).
+}
+
+\keyword{datasets}
