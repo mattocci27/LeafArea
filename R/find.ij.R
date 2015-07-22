0f444f986061a5de2a597227add257e6e963df5f
mattocci27 <mattocci27@gmail.com>
2015-07-22 16:19:13 -0400

    imagej directory

--- R/find.ij.R
+++ R/find.ij.R
@@ -3,7 +3,7 @@ find.ij <- function(ostype = .Platform$OS.type){
 
 	if (ostype == "windows"){
 		if(file.exists("\\Program Files\\ImageJ\\ij.jar")!=T || file.exists("\\Program Files\\ImageJ\\jre\\bin\\java.exe")!=T) {
-			warning("ij.jar or java were not found in the common install location on your system; When you run run.ij, specify the path to ImageJ or try installing ImageJ bundled with Java to C:\\Program Files\\ ")
+			warning("ij.jar or java were not found in the common install location on your system; When you run run.ij, specify the path to ImageJ directory or try installing ImageJ bundled with Java to C:\\Program Files\\ ")
 			return("ImageJ not found")} else return("C:\\Program Files\\ImageJ\\")
 		} else {
 			unix.check <-Sys.info()["sysname"]
