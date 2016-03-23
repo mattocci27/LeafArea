2683af0d844738c6281ca3cf2f73e2d2ed780d7b
mattocci27 <mattocci27@gmail.com>
2016-03-23 15:24:08 -0400

    requires  1.49

--- R/run.ij.R
+++ R/run.ij.R
@@ -8,7 +8,7 @@ if (length(file.list) == 0) return("No images in the directory")
 temp.slash <- substr(set.directory,nchar(set.directory),nchar(set.directory))
 if(temp.slash!="/" & temp.slash!="\\"){
 	set.directory <- paste(set.directory,"/",sep="")
-} 
+}
 
 circ.arg <- paste(low.circ,upper.circ,sep="-")
 size.arg <- paste(low.size,upper.size,sep="-")
@@ -26,14 +26,14 @@ if (os=="windows"){
 	#slash is replaced by backslash because they don't work in batch
 	path.imagej <- gsub("/","\\\\",path.imagej)
 
-	if(file.exists(paste(path.imagej,"ij.jar",sep=""))!=T & file.exists(paste(path.imagej,"ij.jar",sep="/"))!=T) {warning("ij.jar was not found. Specify the correct path to ImageJ directory or reinstall ImageJ bundled with Java") 
+	if(file.exists(paste(path.imagej,"ij.jar",sep=""))!=T & file.exists(paste(path.imagej,"ij.jar",sep="/"))!=T) {warning("ij.jar was not found. Specify the correct path to ImageJ directory or reinstall ImageJ bundled with Java")
 		return("ImageJ not found")
-	}  else if (file.exists(paste(path.imagej,"jre/bin/java.exe",sep=""))!=T & file.exists(paste(path.imagej,"jre/bin/java.exe",sep="/"))!=T) {warning("java was not found. Specify the correct path to ImageJ directory or reinstall ImageJ bundled with Java") 
+	}  else if (file.exists(paste(path.imagej,"jre/bin/java.exe",sep=""))!=T & file.exists(paste(path.imagej,"jre/bin/java.exe",sep="/"))!=T) {warning("java was not found. Specify the correct path to ImageJ directory or reinstall ImageJ bundled with Java")
 		return("ImageJ not found")
 	}
 } else {
 	unix.check <-Sys.info()["sysname"]
-		if(unix.check=="Linux") look <- "ImageJ" else look <- "ImageJ64.app"
+		if(unix.check=="Linux") look <- "ImageJ" else look <- "ImageJ.app"
 	if(file.exists(paste(path.imagej,look,sep=""))!=T & file.exists(paste(path.imagej,look,sep="/"))!=T) {warning("Specify the correct path to ImageJ")
 			return("ImageJ not found")}
 }
@@ -43,7 +43,7 @@ if (os == "windows"){temp <- paste(tempdir(),"\\",sep="")
 temp <- gsub("\\\\","\\\\\\\\",temp)} else {temp <- paste(tempdir(),"/",sep="")
 }
 
-if(save.image==T) macro <- paste('dir = getArgument;\n dir2 = "',temp,'";\n list = getFileList(dir);\n open(dir + list[0]);\n run("Set Scale...", "distance=',distance.pixel, ' known=',known.distance, ' pixel=1 unit=cm global");\n for (i=0;\n i<list.length;\n i++) { open(dir + list[i]);\n width = getWidth() - ',trim.pixel, ';\n height = getHeight() -',trim.pixel,' ;\n run("Canvas Size...", "width=" + width + " height=" + height + " position=Bottom-Center");\n run("8-bit");\n run("Threshold...");\n setAutoThreshold("Minimum");\n run("Analyze Particles...", "size=',size.arg,' circularity=',circ.arg,' show=Masks display clear record");\n saveAs("Measurements", dir2+list[i]+".txt");\n saveAs("tiff", dir+list[i]+ "_mask.tif");\n}',sep="") else macro <- paste('dir = getArgument;\n dir2 = "',temp,'";\n list = getFileList(dir);\n open(dir + list[0]);\n run("Set Scale...", "distance=',distance.pixel, ' known=',known.distance, ' pixel=1 unit=cm global");\n for (i=0;\n i<list.length;\n i++) { open(dir + list[i]);\n width = getWidth() - ',trim.pixel, ';\n height = getHeight() -',trim.pixel,' ;\n run("Canvas Size...", "width=" + width + " height=" + height + " position=Bottom-Center");\n run("8-bit");\n run("Threshold...");\n setAutoThreshold("Minimum");\n run("Analyze Particles...", "size=',size.arg,' circularity=',circ.arg,' show=Masks display clear record");\n saveAs("Measurements", dir2+list[i]+".txt");\n}',sep="") 
+if(save.image==T) macro <- paste('dir = getArgument;\n dir2 = "',temp,'";\n list = getFileList(dir);\n open(dir + list[0]);\n run("Set Scale...", "distance=',distance.pixel, ' known=',known.distance, ' pixel=1 unit=cm global");\n for (i=0;\n i<list.length;\n i++) { open(dir + list[i]);\n width = getWidth() - ',trim.pixel, ';\n height = getHeight() -',trim.pixel,' ;\n run("Canvas Size...", "width=" + width + " height=" + height + " position=Bottom-Center");\n run("8-bit");\n run("Threshold...");\n setAutoThreshold("Minimum");\n run("Analyze Particles...", "size=',size.arg,' circularity=',circ.arg,' show=Masks display clear record");\n saveAs("Measurements", dir2+list[i]+".txt");\n saveAs("tiff", dir+list[i]+ "_mask.tif");\n}',sep="") else macro <- paste('dir = getArgument;\n dir2 = "',temp,'";\n list = getFileList(dir);\n open(dir + list[0]);\n run("Set Scale...", "distance=',distance.pixel, ' known=',known.distance, ' pixel=1 unit=cm global");\n for (i=0;\n i<list.length;\n i++) { open(dir + list[i]);\n width = getWidth() - ',trim.pixel, ';\n height = getHeight() -',trim.pixel,' ;\n run("Canvas Size...", "width=" + width + " height=" + height + " position=Bottom-Center");\n run("8-bit");\n run("Threshold...");\n setAutoThreshold("Minimum");\n run("Analyze Particles...", "size=',size.arg,' circularity=',circ.arg,' show=Masks display clear record");\n saveAs("Measurements", dir2+list[i]+".txt");\n}',sep="")
 
 
 #prepare macro***.txt as tempfile
@@ -63,7 +63,7 @@ if(check.image==T) {
 if (os == "windows"){
 
 	if (length(strsplit(set.directory," ")[[1]]) >1) {
-	bat <- paste("pushd ", path.imagej, "\n jre\\bin\\java -jar -Xmx",set.memory,"g ij.jar ",exe ,tempmacro, ' "',set.directory,'"\n pause\n exit',sep="") 
+	bat <- paste("pushd ", path.imagej, "\n jre\\bin\\java -jar -Xmx",set.memory,"g ij.jar ",exe ,tempmacro, ' "',set.directory,'"\n pause\n exit',sep="")
 	} else bat <- paste("pushd ", path.imagej, "\n jre\\bin\\java -jar -Xmx",set.memory,"g ij.jar ",exe ,tempmacro," ",set.directory,"\n pause\n exit",sep="")
 
 	tempbat <- paste(tempfile('bat'),".bat",sep="")
@@ -76,7 +76,7 @@ if (os == "windows"){
 		temp.slash2 <- substr(path.imagej,nchar(path.imagej),nchar(path.imagej))
 		if(temp.slash2!="/" ){
 			path.imagej <- paste(path.imagej,"/",sep="")
-		} 
+		}
 
 		# this allows space in path
 		set.directory <- gsub(" ", "\\ ", set.directory, fixed=TRUE)
@@ -84,24 +84,24 @@ if (os == "windows"){
 		unix.check <- Sys.info()["sysname"]
 		if(unix.check=="Linux") {
 			system(paste("java -Xmx",set.memory,"g -jar ",path.imagej,"ij.jar -ijpath ",path.imagej," ",exe,tempmacro," ",set.directory,sep=""),wait=wait)
-		} else {system(paste("java -Xmx",set.memory,"g -jar ",path.imagej,"ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath ",path.imagej," ",exe,tempmacro," ",set.directory,sep=""),wait=wait)
+		} else {system(paste("java -Xmx",set.memory,"g -jar ",path.imagej,"ImageJ.app/Contents/Resources/Java/ij.jar -ijpath ",path.imagej," ",exe,tempmacro," ",set.directory,sep=""),wait=wait)
 }
 }
 
 # system(paste("java -Xmx",set.memory,"g -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ ",exe,tempmacro," ",set.directory,sep=""),wait=wait)
-# system(paste("java -Xmx",set.memory,"g -jar ", pathimagej, "/ij.jar  -macro ",tempmacro," ",set.directory,sep=""),wait=F)		
+# system(paste("java -Xmx",set.memory,"g -jar ", pathimagej, "/ij.jar  -macro ",tempmacro," ",set.directory,sep=""),wait=F)
 
 
 #kill imageJ
 if(check.image==T){
 	ans <- readline("Do you want to close ImageJ? Press any keys when you finish cheking analyzed images.")
-	if (os == "windows") suppressWarnings(shell('taskkill /f /im "java.exe"')) else system("killall java")	
+	if (os == "windows") suppressWarnings(shell('taskkill /f /im "java.exe"')) else system("killall java")
 }
-	
+
 # file managemanet
-	res <- resmerge.ij(path=temp,prefix=prefix) 
-	
-	if (log==T) res2 <- readtext.ij(path=temp) 
+	res <- resmerge.ij(path=temp,prefix=prefix)
+
+	if (log==T) res2 <- readtext.ij(path=temp)
 
 # unlink
 	cd <- getwd()
@@ -110,7 +110,7 @@ if(check.image==T){
 	setwd(cd)
 
 	if (log==T) return(list(summary=res,each.image=res2)) else return(res)
-	
+
 }
 
 
@@ -121,4 +121,4 @@ if(check.image==T){
 # res <- auto.run(set.directory=moge,log=T)
 
 
-# system(paste("java -Xmx",set.memory,"g -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ"," -macro ~/Desktop/moge.txt ",set.directory,sep=""))	
+# system(paste("java -Xmx",set.memory,"g -jar /Applications/ImageJ/ImageJ64.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ"," -macro ~/Desktop/moge.txt ",set.directory,sep=""))
