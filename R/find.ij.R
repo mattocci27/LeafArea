6ff065079faf288114b0635fdb443dc128e52a4b
mattocci27 <mattocci27@gmail.com>
2016-03-23 15:06:56 -0400

    update find.ij
    now using both ImageJ.app and ImageJ64.app on Mac

--- R/find.ij.R
+++ R/find.ij.R
@@ -1,27 +1,20 @@
 ##works in Mac
 find.ij <- function(ostype = .Platform$OS.type){
 
-	if (ostype == "windows"){
-		if(file.exists("\\Program Files\\ImageJ\\ij.jar")!=T || file.exists("\\Program Files\\ImageJ\\jre\\bin\\java.exe")!=T) {
-			warning("ij.jar or java were not found in the common install location on your system; When you run run.ij, specify the path to ImageJ directory or try installing ImageJ bundled with Java to C:\\Program Files\\ ")
-			return("ImageJ not found")} else return("C:\\Program Files\\ImageJ\\")
-		} else {
-			unix.check <-Sys.info()["sysname"]
-		if(unix.check=="Linux") {
-			warning("Specify the path to ImageJ")	
-		return("ImageJ not found")} else {
+  if (ostype == "windows"){
+    if(file.exists("\\Program Files\\ImageJ\\ij.jar")!=T || file.exists("\\Program Files\\ImageJ\\jre\\bin\\java.exe")!=T) {
+      warning("ij.jar or java were not found in the common install location on your system; When you run run.ij, specify the path to ImageJ directory or try installing ImageJ bundled with Java to C:\\Program Files\\ ")
+      return("ImageJ not found")} else return("C:\\Program Files\\ImageJ\\")
+    } else {
+      unix.check <-Sys.info()["sysname"]
+    if(unix.check=="Linux") {
+      warning("Specify the path to ImageJ")
+    return("ImageJ not found")} else {
 
-		if(file.exists("/Applications/ImageJ/ImageJ64.app")!=T){
-		warning("ImageJ was not found in the common install location on your system; When you run run.ij, specify the path to ImageJ or try installing ImageJ to /Applications/ImageJ/")	
-		return("ImageJ not found")
-		} else return("/Applications/ImageJ/")			
-		}
-			}
+    if(file.exists("/Applications/ImageJ/ImageJ.app")!=T & file.exists("/Applications/ImageJ/ImageJ64.app")!=T){
+    warning("ImageJ was not found in the common install location on your system; When you run run.ij, specify the path to ImageJ or try installing ImageJ to /Applications/ImageJ/")
+    return("ImageJ not found")
+    } else return("/Applications/ImageJ/")
+    }
+      }
 }
-
-
-
-
-
-
-
