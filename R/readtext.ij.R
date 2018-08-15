9e399828023721f01f84942d494f293d0b7dc1de
Masatoshi Katabuchi <mattocci27@gmail.com>
2018-08-14 21:39:35 -0400

    remove peri

--- R/readtext.ij.R
+++ R/readtext.ij.R
@@ -17,7 +17,8 @@ readtext.ij <- function(path){
 
   for (i in 1:length(file.list)){
     temp <- read.delim(paste(path, file.list[i], sep = ""))
-    data[[i]] <- data.frame(Area = temp$Area, Perim. = temp$Perim.)
+    #data[[i]] <- data.frame(Area = temp$Area, Perim. = temp$Perim.)
+    data[[i]] <- data.frame(Area = temp$Area)
     # data[[i]] <- read.delim(paste(path,file.list[i],sep=""))
     names(data)[i] <- paste(file.list[i])
     }
--- R/resmerge.ij.R
+++ R/resmerge.ij.R
@@ -3,15 +3,17 @@
 resmerge.ij <- function(path, prefix = "\\.|-"){
   temp0 <- readtext.ij(path)
   temp_area <- sapply(sapply(temp0, "[", 1), sum)
-  temp_perim <- sapply(sapply(temp0, "[", 2), sum)
+  #temp_perim <- sapply(sapply(temp0, "[", 2), sum)
 
   temp.data <- data.frame(file.name = names(temp_area),
-            size = temp_area,
-            perim = temp_perim)
+            size = temp_area)
+            #perim = temp_perim)
   temp.data$file.name <- factor(sapply(strsplit(as.character(temp.data$file.name), prefix), "[",1))
   # return(temp.data)
-  res <- aggregate(list(temp.data["size"], temp.data["perim"]),
-    temp.data["file.name"], sum)
-  names(res) <- c("sample", "total.leaf.area", "perimeter")
+  #res <- aggregate(list(temp.data["size"], temp.data["perim"]),
+  #  temp.data["file.name"], sum)
+  res <- aggregate(temp.data["size"],temp.data["file.name"],sum)
+  #names(res) <- c("sample", "total.leaf.area", "perimeter")
+  names(res) <- c("sample", "total.leaf.area")
   return(res)
 }
