94a873a45402a9d3b1077d6c700092f44d2af2dd
Masatoshi Katabuchi <mattocci27@gmail.com>
2019-07-01 16:19:30 +0800

    clean readability

--- R/find.ij.R
+++ R/find.ij.R
@@ -1,21 +1,20 @@
 ##works in Mac
 find.ij <- function(ostype = .Platform$OS.type){
-
   if (ostype == "windows"){
-    if(file.exists("\\Program Files\\ImageJ\\ij.jar")!=T || file.exists("\\Program Files\\ImageJ\\jre\\bin\\java.exe")!=T) {
+    if (file.exists("\\Program Files\\ImageJ\\ij.jar") != T || file.exists("\\Program Files\\ImageJ\\jre\\bin\\java.exe") != T) {
       warning("ij.jar or java were not found in the common install location on your system; When you run run.ij, specify the path to ImageJ directory or try installing ImageJ bundled with Java to C:\\Program Files\\ ")
       return("ImageJ not found")} else return("C:\\Program Files\\ImageJ\\")
     } else {
-      unix.check <-Sys.info()["sysname"]
-    if(unix.check=="Linux") {
+      unix.check <- Sys.info()["sysname"]
+    if (unix.check == "Linux") {
       warning("Specify the path to the directory that contains ImageJ.app and ij.jar")
-    return("ImageJ not found")} else {
-
-    imagej <- system("mdfind -name ImageJ | grep 'ImageJ.app'", intern = TRUE)
-    if(length(imagej) != 0) return(imagej) else {
-      warning("ImageJ.app was not found in the common install location on your system; When you run run.ij, specify the path to ImageJ.app")
-      return("ImageJ not found")
-          }
+    return("ImageJ not found")
+    } else {
+      imagej <- system("mdfind -name ImageJ | grep 'ImageJ.app'", intern = TRUE)
+      if(length(imagej) != 0) return(imagej) else {
+        warning("ImageJ.app was not found in the common install location on your system; When you run run.ij, specify the path to ImageJ.app")
+        return("ImageJ not found")
+            }
       }
     }
 }
--- R/resmerge.ij.R
+++ R/resmerge.ij.R
@@ -1,11 +1,15 @@
 #' @importFrom stats aggregate
 
-resmerge.ij <- function(path,prefix="\\.|-"){
+resmerge.ij <- function(path, prefix = "\\.|-"){
   temp0 <- readtext.ij(path)
-  temp <- sapply(temp0,sum)
-  temp.data <- data.frame(file.name=names(temp),size=temp)
-  temp.data$file.name <-factor(sapply(strsplit(as.character(temp.data$file.name), prefix), "[",1))
-  res <- aggregate(temp.data["size"],temp.data["file.name"],sum)
-  names(res) <- c("sample","total.leaf.area")
+  temp <- sapply(temp0, sum)
+  temp.data <- data.frame(file.name = names(temp), size = temp)
+  #temp.data$file.name <- factor(sapply(strsplit(as.character(temp.data$file.name), prefix), "[", 1))
+
+  tmp1 <- strsplit(as.character(temp.data$file.name), prefix)
+  tmp1 <- sapply(tmp1, "[", 1)
+  temp.data$file.name <- factor(tmp1)
+  res <- aggregate(temp.data["size"], temp.data["file.name"], sum)
+  names(res) <- c("sample", "total.leaf.area")
   return(res)
 }
