c8c112d1c11782f6d2062f968cb78748cc30a76a
Masatoshi Katabuchi <mattocci27@gmail.com>
2019-07-03 15:13:18 +0800

    style: roxygen2

---
+++ R/leafdata.r
@@ -0,0 +1,9 @@
+#' LeafArea default data
+#'
+#' Analyzed leaf area data for example
+#'
+#' @docType data
+#' @keywords data
+#' @format A list with six data frames, A1-01.jpeg.txt, A1-02.jpeg.txt, A123-01.jpeg.txt, A123-02.jpeg.txt, A2.jpeg.txt and A300-1.jpeg.txt. The first column is the identity of image, the second column is the area of the image (cm2) and the thrid column is the perimeter of the image (cm).
+#' @name leafdata
+"leafdata"
--- R/resmerge.ij.R
+++ R/resmerge.ij.R
@@ -1,4 +1,39 @@
+#' File management
+#'
+#' File management function. The output file contains sample names in the first column and total leaf area (cm2) of the sample (e.g., one individual plant or one ramet) in the second column.
+#'
 #' @importFrom stats aggregate
+#' @author Masatoshi Katabuchi \email{mattocci27@gmail.com}
+#' @export
+#' @param path Path to the target directory
+#' @param prefix Regular expression to manage file names
+#' @return A data frame of total leaf area for each sample
+#' @return \item{sample}{Name of sample}
+#' @return \item{total.leaf.area}{Total leaf area of the sample (cm2)}
+#' @examples
+#' #prepare example files
+#' data(leafdata)
+#' tf <- paste(tempdir(),"/",sep="")
+#' for (i in 1:7){
+#' 	write.table(leafdata[[i]],paste(tf,names(leafdata)[i],sep=""),sep="\t")
+#' }
+#' 
+#' #list of files
+#' list.files(tf)
+#' 
+#' #combine multiple tab-delimited text files with a leaf area value
+#' #(one text file for each original JPEG image file) that share the same
+#' #filename 'prefix', defined as the part of the filename preceding the first
+#' #hyphen (-) or period (.).
+#' resmerge.ij(tf)
+#' 
+#' #combine multiple tab-delimited text files with a leaf area value
+#' #(one text file for each original JPEG image file) that share the same
+#' #filename 'prefix', defined as the part of the filename preceding the first
+#' #'.txt'.
+#' resmerge.ij(tf, prefix = ".txt")
+#' 
+#' unlink(list.files(tf))
 
 resmerge.ij <- function(path, prefix = "\\.|-"){
   temp0 <- readtext.ij(path)
